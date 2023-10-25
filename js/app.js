dbaba2fd6e40c4d94e8af0d3fa4ea30e6747ffcc
LucaLF-dev <laficaraluca@gmail.com>
2023-10-25 14:54:38 +0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+const { createApp } = Vue 
+
+createApp({
+    data(){
+        return{
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+
+        }
+    },
+    methods(){
+
+    },
+    mounted(){
+        console.log('VUE OK')
+    },
+
+}).mount('#app')
