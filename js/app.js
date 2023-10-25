8b409c7557ff74c3974eb2d0a947f4ae58c79e81
LucaLF-dev <laficaraluca@gmail.com>
2023-10-25 18:28:59 +0200

    Add - bonus 2 and fixed input newtask min caracter

--- js/app.js
+++ js/app.js
@@ -1,43 +1,54 @@
-
-const { createApp } = Vue 
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            newTask:'',
-            todos: [
-                {
-                    text: 'Fare i compiti',
-                    done: false
-                },
-                {
-                    text: 'Fare la spesa',
-                    done: true
-                },
-                {
-                    text: 'Fare il bucato',
-                    done: false
-                }
-            ]
+	data() {
+		return {
+			newTask: "",
+			todos: [
+				{
+					text: "Fare i compiti",
+					done: false,
+				},
+				{
+					text: "Fare la spesa",
+					done: true,
+				},
+				{
+					text: "Fare il bucato",
+					done: false,
+				},
+			],
+			error: false,
+		};
+	},
+	methods: {
+		removeTask(task) {
+			this.todos.splice(task, 1);
+		},
 
-        }
-    },
-    methods: {
-       removeTask(task) {
-         this.todos.splice(task,1)
-       },
-
-       addTask() {
-        const newTodo = {
-            text : this.newTask,
-            done : false
-        }
-        this.todos.splice(0,0,newTodo)
-       }
-
-    },
-    mounted(){
-        console.log('VUE OK')
-    },
-
-}).mount('#app')
+		addTask() {
+			if (this.newTask.length >= 5) {
+				this.error = false;
+                const newTodo = {
+					text: this.newTask,
+					done: false,
+				};
+				this.todos.splice(0, 0, newTodo);
+				this.newTask = "";
+				
+			} else {
+				this.error = true;
+			}
+		},
+		taskCheck(taskIndex) {
+			if (this.todos[taskIndex].done === false) {
+				this.todos[taskIndex].done = true;
+			} else if (this.todos[taskIndex].done === true) {
+				this.todos[taskIndex].done = false;
+			}
+		},
+	},
+	mounted() {
+		console.log("VUE OK");
+	},
+}).mount("#app");
